cee27fcd963ffe91370d0aaee009ddc9f448551e
Jaydip Patel <jdpatel4405@gmail.com>
2022-02-11 10:01:10 +0530

    11-02-2022 commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const express = require("express");
+const sessionController = require("./controller/session-controller")
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.write("Hello world This is main page")
+    res.end()
+})
+
+app.get("/login", sessionController.login);
+app.get("/signup", sessionController.signup);
+
+// app.get('/login', (req, res) => {
+//     res.write("Login Page")
+//     res.end()
+// })
+// app.get('/signup', (req, res) => {
+//     res.sendFile('/views/signup.html', { root: __dirname });
+// })
+
+app.listen(3000, () => {
+    console.log("This is callback function");
+})
